cd049fd7b658d621b1781c11ff47125df801f7eb
Yeuem2003 <tongducduy4@gmail.com>
2023-03-25 16:44:42 +0700

    End of section 10

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_module/member';
+import { map, of } from 'rxjs';
 
 
 @Injectable({
@@ -9,13 +10,30 @@ import { Member } from '../_module/member';
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl+ 'users')
+    if(this.getMembers.length >0 ) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl+ 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    )
   }
   getMember(username: string){
+    const member = this.members.find(x => x.username === username);
+    if(member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username)
+  }
+  updateMember(member: Member){
+    return this.http.put(this.baseUrl +'users',member).pipe(
+      map(() =>{
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    );
   }
 }
